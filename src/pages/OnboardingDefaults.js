f5800c07e5bb6bea4db810f8381ff0c10625f83c
Ulises Gomez <ulisesgomez120@gmail.com>
2021-04-03 20:41:08 -0500

    added google-spreadsheet for api writes

--- src/pages/OnboardingDefaults.js
+++ src/pages/OnboardingDefaults.js
@@ -1,35 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { GoogleSpreadsheet } from "google-spreadsheet";
-
-// Config variables
-const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
-const SHEET_ID = process.env.REACT_APP_SHEET_ID;
-const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
-const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
-
-const appendSpreadsheet = async (row) => {
-  try {
-    await doc.useServiceAccountAuth({
-      client_email: CLIENT_EMAIL,
-      private_key: PRIVATE_KEY,
-    });
-    // loads document properties and worksheets
-    await doc.loadInfo();
-    console.log(doc.title);
-    const sheet = doc.sheetsByIndex[0];
-    console.log(sheet.title);
-    const result = await sheet.addRow(row);
-  } catch (e) {
-    console.error("Error: ", e);
-  }
-};
-
-const newRow = { Ticker: "new name", Name: "new value" };
 
 const OnboardingDefaults = () => {
-  appendSpreadsheet(newRow);
   return (
     <section>
       <h2>onboard/update</h2>
---
+++ src/util/sheets.js
@@ -0,0 +1,25 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+// Config variables
+const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
+const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+export const addRow = async (row) => {
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: CLIENT_EMAIL,
+      private_key: PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    // loads document properties and worksheets
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByIndex[0];
+
+    const result = await sheet.addRow(row);
+  } catch (e) {
+    console.error("Error: ", e);
+  }
+};
